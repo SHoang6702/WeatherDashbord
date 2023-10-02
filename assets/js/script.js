f34bdb4c6833ac9b755c0533d40060548b240c8f
SHoang6702 <shoang6702@gmail.com>
2023-10-02 17:15:39 -0500

    Local storage breaking

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// API Request non functional during testing, commented out to not break code.
 // var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={bc3ebe5a0464504aa9d5eeb2d0ad5af1}'
 // function getApi(requestUrl) {
 //     fetch(requestUrl).then(function (response) {
@@ -7,25 +8,31 @@
 // }
 // getApi(requestUrl)
 
+
+//Local storage is returning Null object
 let cityInput = document.getElementById('city')
-let searchInput = document.getElementById('searchButton')
-searchInput.addEventListener('click',
-    function(event) {
-    const cityInput = document.getElementById('cityInput')
-    localStorage.setItem('cityInput', JSON.stringify(cityInput))
-    console.log(localStorage.getItem(cityInput))
-})
+let searchButton = document.getElementById('searchButton')
+searchButton.addEventListener('click', 
+function (event) {
+  const cityHistory = document.querySelector('#city');
+  const searchedHistory = document.querySelector('#searchHistory');
+  //only saves one city
+  localStorage.setItem('searchHistory', cityHistory.value);
+  console.log(localStorage.getItem('searchHistory'));
+  //code below non functional
+  searchedHistory.innerHTML = localStorage.getItem('searchHistory');
+});
 
 // GIVEN a weather dashboard with form inputs
 
 // WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// THEN I am presented with current and future conditions for that city and that city is added to the search history (WIP)
 
 // WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed (WIP)
 
 // WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity (WIP)
 
 // WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
+// THEN I am again presented with current and future conditions for that city 
