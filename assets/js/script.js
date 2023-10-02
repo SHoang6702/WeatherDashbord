6f841259aabb94fa6f2bf80ae970b045b3a2af8d
SHoang6702 <shoang6702@gmail.com>
2023-10-02 17:54:34 -0500

    cleaned up WIP comments.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 // API Request non functional during testing, commented out to not break code.
+//link or key is potentially filled out wrong, lattitude and longitude are currently undefined so may be current issue with API, unsure how to make it search via lat and long.
+//need to reread the api documentation to find solution
 
 // var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={bc3ebe5a0464504aa9d5eeb2d0ad5af1}'
 // function getApi(requestUrl) {
@@ -11,24 +13,26 @@
 
 //API still not working can not proceed until fixed, commented out to not break storage code.
 
-//Local storage is returning Null object
+//Local storage is returning Null object (fixed, error was a typo)
 let cityInput = document.getElementById('city')
 let searchButton = document.getElementById('searchButton')
 searchButton.addEventListener('click', 
 function (event) {
   const cityHistory = document.querySelector('#city');
   const searchHistory = document.querySelector('#searchHistory');
-  //only saves one city, JSON.stringify might work, but can't figure it out.
+  //only saves one city, JSON.stringify might work, but can't figure it out. (More work is needed but API issue is more pressing, repair this part last)
   localStorage.setItem('searchHistory', cityHistory.value);
   console.log(localStorage.getItem('searchHistory'));
   //code below non functional innerHTML error (fixed, thanks to typo)
   searchHistory.innerHTML = localStorage.getItem('searchHistory');
 });
-//nonfunction below, experimental
+
+//nonfunction below, experimental (Intend to use once API is working.)
 let historyButton = document.getElementById('searchHistory');
 historyButton.addEventListener('click',
 function (event) {
 const cityHistory = document.querySelector('#city')
+//not working as intended, but can be modified to load up if API begins to work, API is still unavailable. Unsure of why.
 cityHistory.innerHTML = localStorage.getItem('searchHistory');
 });
 // note to self: history is not saving on refresh, needs repair. (fixed, but button is still not functional, still only saving 1 city.)
@@ -37,6 +41,8 @@ searchHistory.innerHTML = localStorage.getItem('searchHistory');
 }
 //fixes issue of website not saving on refresh.
 reload();
+
+//## Acceptance Criteria
 
 // GIVEN a weather dashboard with form inputs
 
