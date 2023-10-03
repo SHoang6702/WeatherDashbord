5cd4c1f3382f813b6b71df876131bea8fa5e9b09
SHoang6702 <shoang6702@gmail.com>
2023-10-02 21:33:34 -0500

    adding comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
 // API Request non functional during testing, commented out to not break code.
 //link or key is potentially filled out wrong, lattitude and longitude are currently undefined so may be current issue with API, unsure how to make it search via lat and long.
 //need to reread the api documentation to find solution
+
+//below was a test using variables attempting to concatinate (however you spell that word), was unsuccessful
 // var lon = 0
 // var lat = 0
 // var apiKey = '7cced79f0ce1da9ff3598003fe1b9f85'
 // var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apiKey}'
 
+//atempting to hard code the api key in, issue may be in the empty longitude/lattitude variables?
 var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={7cced79f0ce1da9ff3598003fe1b9f85}'
 function getApi(requestUrlArg) {
     fetch(requestUrlArg).then(function (response) {
